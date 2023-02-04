74f2b35b337b2ce01fd567ee7ec47050b788780f
IDCDEV03 <idcdev03@iddrives.co.th>
2023-02-04 16:51:56 +0700

    ีupdate

--- resources/views/userpages/all_packages.blade.php
+++ resources/views/userpages/all_packages.blade.php
@@ -20,91 +20,112 @@
 
                 <div class="card">
                     <div class="card-body">
-                        <a href="{{route('private_package',['id' => Auth::user()->id])}}" class="btn btn-lg btn-secondary" type="button">จัดกรุ๊ปทัวร์ส่วนตัว</a>
-                    </div>
-                    </div>
-
-        @foreach ($all_packages as $row)
-        <!--------------tab1---------------->
-        
-            <div class="col-xl-3 col-sm-6 xl-4">
-                <div class="card">
-
-                    <div class="product-box">
-                        <div class="product-img">
-                            <img class="img-fluid" src="{{ asset($row->package_cover)}}" alt="">
-                            <div class="product-hover">
-                                <ul>                                   
-                                    <li>
-                                        <button class="btn" type="button" data-bs-toggle="modal"
-                                      data-bs-target="#Modal_{{$row->package_id}}"><i class="icon-eye"></i></button>
-                                    </li>                                 
-                                </ul>
+                        <div class="row">
+                            <div class="col-md-4 text-center">
+                                <a href="{{ route('private_package', ['id' => Auth::user()->id]) }}"
+                                    class="btn btn-lg btn-secondary" type="button">จัดกรุ๊ปทัวร์ส่วนตัว</a>
                             </div>
-                        </div>
-                        <div class="modal fade" id="Modal_{{$row->package_id}}" tabindex="-1" role="dialog"
-                            aria-labelledby="Modal_{{$row->package_id}}" aria-hidden="true">
-                            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <div class="product-box row">
-                                            <div class="product-img col-lg-6"><img class="img-fluid"
-                                                    src="{{ asset($row->package_cover)}}" alt=""></div>
-                                            <div class="product-details col-lg-6 text-start">
-                                                <h4>{{$row->package_name}}</h4>
-                                                <div class="product-price">{{number_format($row->package_price)}}.-
-                                                </div>
-                                                <div class="product-view">
-                                                    <h6 class="f-w-600">รายละเอียด</h6>
-                                                
-                                                     <p class="mb-0">
-                                                        ไฮไลท์ทัวร์ : {{$row->highlight_tour}}    
-                                                     </p>
-                                                     <p class="mb-0">
-                                                     ระยะเวลา :  {{$row->package_total_day}}    
-                                                     </p>
-                                            
-                                                   <p class="mb-0">
-                                                    ดาวน์โหลดรายละเอียดโปรแกรม : 
-                                                    <a href="{{ asset($row->package_file)}}">คลิก</a>
-                                                    </p>
-                                                </div>
-                                             <br>
-                                                <div class="product-qnty">
-                                                    <div class="addcart-btn">
-                                                <a
-                                                href="{{route('book_package',['id'=>Auth::user()->id,'pkid'=>$row->package_id]);}}" class="btn btn-primary" type="button">สั่งจองแพ็คเกจนี้</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <button class="btn-close" type="button" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
+                            <div class="col-md-8">
+                 <form action="" >
+                        <div class="row">
+            <div class="col-md-6 text-center">
+                <input class="form-control" placeholder="ค้นหาทัวร์ (ชื่อเมือง หรือ ประเทศ)" name="search" type="search" value="{{$search}}">
+            </div>
+            <div class="col-md-6 text-center">
+                <button type="submit" class="btn btn-primary">Search</button>
+                <a href="{{route('user.all_packages')}}" class="btn btn-info"
+                type="button">Reset</a>
+            </div>
                                     </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
-
-                        <div class="product-details">                         
-                            <a href="#" data-bs-toggle="modal"
-                            data-bs-target="#Modal_{{$row->package_id}}">
-                            <h4>
-                                {{$row->package_name}}</h4>
-                            </a>
-                            <p>{{$row->package_total_day}} / {{$row->package_place}}</p>
-                            <div class="product-price">{{number_format($row->package_price)}}.-
-                                
-                            </div>
-                        </div>
-
                     </div>
                 </div>
-            </div>        
-        <!--------------tab1---------------->
-  
-@endforeach
-</div>
- </div>
+
+                @foreach ($all_packages as $row)
+                    <!--------------tab1---------------->
+
+                    <div class="col-xl-3 col-sm-6 xl-4">
+                        <div class="card">
+
+                            <div class="product-box">
+                                <div class="product-img">
+                                    <img class="img-fluid" src="{{ asset($row->package_cover) }}" alt="">
+                                    <div class="product-hover">
+                                        <ul>
+                                            <li>
+                                                <button class="btn" type="button" data-bs-toggle="modal"
+                                                    data-bs-target="#Modal_{{ $row->package_id }}"><i
+                                                        class="icon-eye"></i></button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="modal fade" id="Modal_{{ $row->package_id }}" tabindex="-1" role="dialog"
+                                    aria-labelledby="Modal_{{ $row->package_id }}" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <div class="product-box row">
+                                                    <div class="product-img col-lg-6"><img class="img-fluid"
+                                                            src="{{ asset($row->package_cover) }}" alt=""></div>
+                                                    <div class="product-details col-lg-6 text-start">
+                                                        <h4>{{ $row->package_name }}</h4>
+                                                        <div class="product-price">
+                                                            {{ number_format($row->package_price) }}.-
+                                                        </div>
+                                                        <div class="product-view">
+                                                            <h6 class="f-w-600">รายละเอียด</h6>
+
+                                                            <p class="mb-0">
+                                                                ไฮไลท์ทัวร์ : {{ $row->highlight_tour }}
+                                                            </p>
+                                                            <p class="mb-0">
+                                                                ระยะเวลา : {{ $row->package_total_day }}
+                                                            </p>
+
+                                                            <p class="mb-0">
+                                                                ดาวน์โหลดรายละเอียดโปรแกรม :
+                                                                <a href="{{ asset($row->package_file) }}">คลิก</a>
+                                                            </p>
+                                                        </div>
+                                                        <br>
+                                                        <div class="product-qnty">
+                                                            <div class="addcart-btn">
+                                                                <a href="{{ route('book_package', ['id' => Auth::user()->id, 'pkid' => $row->package_id]) }}"
+                                                                    class="btn btn-primary"
+                                                                    type="button">สั่งจองแพ็คเกจนี้</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <button class="btn-close" type="button" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-details">
+                                    <a href="#" data-bs-toggle="modal"
+                                        data-bs-target="#Modal_{{ $row->package_id }}">
+                                        <h4>
+                                            {{ $row->package_name }}</h4>
+                                    </a>
+                                    <p>{{ $row->package_total_day }} / {{ $row->package_place }}</p>
+                                    <div class="product-price">{{ number_format($row->package_price) }}.-
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!--------------tab1---------------->
+                @endforeach
+            </div>
+        </div>
 
     </div>
     </div>
--- storage/framework/views/5bd6e1bc6e45229137e9084c0c43880518af6eb3.php
+++ storage/framework/views/5bd6e1bc6e45229137e9084c0c43880518af6eb3.php
@@ -18,91 +18,114 @@
 
                 <div class="card">
                     <div class="card-body">
-                        <a href="<?php echo e(route('private_package',['id' => Auth::user()->id])); ?>" class="btn btn-lg btn-secondary" type="button">จัดกรุ๊ปทัวร์ส่วนตัว</a>
-                    </div>
-                    </div>
-
-        <?php $__currentLoopData = $all_packages; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $row): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <!--------------tab1---------------->
-        
-            <div class="col-xl-3 col-sm-6 xl-4">
-                <div class="card">
-
-                    <div class="product-box">
-                        <div class="product-img">
-                            <img class="img-fluid" src="<?php echo e(asset($row->package_cover)); ?>" alt="">
-                            <div class="product-hover">
-                                <ul>                                   
-                                    <li>
-                                        <button class="btn" type="button" data-bs-toggle="modal"
-                                      data-bs-target="#Modal_<?php echo e($row->package_id); ?>"><i class="icon-eye"></i></button>
-                                    </li>                                 
-                                </ul>
+                        <div class="row">
+                            <div class="col-md-4 text-center">
+                                <a href="<?php echo e(route('private_package', ['id' => Auth::user()->id])); ?>"
+                                    class="btn btn-lg btn-secondary" type="button">จัดกรุ๊ปทัวร์ส่วนตัว</a>
                             </div>
-                        </div>
-                        <div class="modal fade" id="Modal_<?php echo e($row->package_id); ?>" tabindex="-1" role="dialog"
-                            aria-labelledby="Modal_<?php echo e($row->package_id); ?>" aria-hidden="true">
-                            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <div class="product-box row">
-                                            <div class="product-img col-lg-6"><img class="img-fluid"
-                                                    src="<?php echo e(asset($row->package_cover)); ?>" alt=""></div>
-                                            <div class="product-details col-lg-6 text-start">
-                                                <h4><?php echo e($row->package_name); ?></h4>
-                                                <div class="product-price"><?php echo e(number_format($row->package_price)); ?>.-
-                                                </div>
-                                                <div class="product-view">
-                                                    <h6 class="f-w-600">รายละเอียด</h6>
-                                                
-                                                     <p class="mb-0">
-                                                        ไฮไลท์ทัวร์ : <?php echo e($row->highlight_tour); ?>    
-                                                     </p>
-                                                     <p class="mb-0">
-                                                     ระยะเวลา :  <?php echo e($row->package_total_day); ?>    
-                                                     </p>
-                                            
-                                                   <p class="mb-0">
-                                                    ดาวน์โหลดรายละเอียดโปรแกรม : 
-                                                    <a href="<?php echo e(asset($row->package_file)); ?>">คลิก</a>
-                                                    </p>
-                                                </div>
-                                             <br>
-                                                <div class="product-qnty">
-                                                    <div class="addcart-btn">
-                                                <a
-                                                href="<?php echo e(route('book_package',['id'=>Auth::user()->id,'pkid'=>$row->package_id])); ?>" class="btn btn-primary" type="button">สั่งจองแพ็คเกจนี้</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <button class="btn-close" type="button" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
+                            <div class="col-md-8">
+                 <form action="" >
+                        <div class="row">
+            <div class="col-md-6 text-center">
+                <input class="form-control" placeholder="ค้นหาทัวร์ (ชื่อเมือง หรือ ประเทศ)" name="search" type="search" value="<?php echo e($search); ?>">
+            </div>
+            <div class="col-md-6 text-center">
+                <button type="submit" class="btn btn-primary">Search</button>
+                <a href="<?php echo e(route('user.all_packages')); ?>" class="btn btn-info"
+                type="button">Reset</a>
+            </div>
                                     </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
-
-                        <div class="product-details">                         
-                            <a href="#" data-bs-toggle="modal"
-                            data-bs-target="#Modal_<?php echo e($row->package_id); ?>">
-                            <h4>
-                                <?php echo e($row->package_name); ?></h4>
-                            </a>
-                            <p><?php echo e($row->package_total_day); ?> / <?php echo e($row->package_place); ?></p>
-                            <div class="product-price"><?php echo e(number_format($row->package_price)); ?>.-
-                                
-                            </div>
-                        </div>
-
                     </div>
                 </div>
-            </div>        
-        <!--------------tab1---------------->
-  
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-</div>
- </div>
+
+                <?php $__currentLoopData = $all_packages; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $row): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <!--------------tab1---------------->
+
+                    <div class="col-xl-3 col-sm-6 xl-4">
+                        <div class="card">
+
+                            <div class="product-box">
+                                <div class="product-img">
+                                    <img class="img-fluid" src="<?php echo e(asset($row->package_cover)); ?>" alt="">
+                                    <div class="product-hover">
+                                        <ul>
+                                            <li>
+                                                <button class="btn" type="button" data-bs-toggle="modal"
+                                                    data-bs-target="#Modal_<?php echo e($row->package_id); ?>"><i
+                                                        class="icon-eye"></i></button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="modal fade" id="Modal_<?php echo e($row->package_id); ?>" tabindex="-1" role="dialog"
+                                    aria-labelledby="Modal_<?php echo e($row->package_id); ?>" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <div class="product-box row">
+                                                    <div class="product-img col-lg-6"><img class="img-fluid"
+                                                            src="<?php echo e(asset($row->package_cover)); ?>" alt=""></div>
+                                                    <div class="product-details col-lg-6 text-start">
+                                                        <h4><?php echo e($row->package_name); ?></h4>
+                                                        <div class="product-price">
+                                                            <?php echo e(number_format($row->package_price)); ?>.-
+                                                        </div>
+                                                        <div class="product-view">
+                                                            <h6 class="f-w-600">รายละเอียด</h6>
+
+                                                            <p class="mb-0">
+                                                                ไฮไลท์ทัวร์ : <?php echo e($row->highlight_tour); ?>
+
+                                                            </p>
+                                                            <p class="mb-0">
+                                                                ระยะเวลา : <?php echo e($row->package_total_day); ?>
+
+                                                            </p>
+
+                                                            <p class="mb-0">
+                                                                ดาวน์โหลดรายละเอียดโปรแกรม :
+                                                                <a href="<?php echo e(asset($row->package_file)); ?>">คลิก</a>
+                                                            </p>
+                                                        </div>
+                                                        <br>
+                                                        <div class="product-qnty">
+                                                            <div class="addcart-btn">
+                                                                <a href="<?php echo e(route('book_package', ['id' => Auth::user()->id, 'pkid' => $row->package_id])); ?>"
+                                                                    class="btn btn-primary"
+                                                                    type="button">สั่งจองแพ็คเกจนี้</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <button class="btn-close" type="button" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-details">
+                                    <a href="#" data-bs-toggle="modal"
+                                        data-bs-target="#Modal_<?php echo e($row->package_id); ?>">
+                                        <h4>
+                                            <?php echo e($row->package_name); ?></h4>
+                                    </a>
+                                    <p><?php echo e($row->package_total_day); ?> / <?php echo e($row->package_place); ?></p>
+                                    <div class="product-price"><?php echo e(number_format($row->package_price)); ?>.-
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!--------------tab1---------------->
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </div>
+        </div>
 
     </div>
     </div>
